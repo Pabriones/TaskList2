48ac2bd50d74c68db3dc91160881838ecef34d88
Pabriones <Pabriones@users.noreply.github.com>
2019-06-13 23:13:10 -0700

    Initial commit

---
+++ app/src/main/java/com/dark_matter/pabri/tasklist2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dark_matter.pabri.tasklist2;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
